2d235614554bb8e143ddf45649dff2558d8e6463
Jason Liang <jasonliang512@gmail.com>
2021-02-24 19:29:36 -0800

    update shadergen

--- src/shadergen.cpp
+++ src/shadergen.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -6,12 +7,19 @@
 #include "language.h"
 
 global FILE *output;
+global i32 count = 0;
 
 internal void MakeShader(const char *in, const char *globalVar)
 {
     fprintf(output, "global const char *%s = \"\\\n", globalVar);
 
     FILE *f = fopen(in, "r");
+    if (!f)
+    {
+        fprintf(stderr, "Can't open %s\n", in);
+        fclose(output);
+        exit(1);
+    }
 
     char line[4096];
     while (fgets(line, ArrayCount(line), f))
@@ -23,17 +31,17 @@ internal void MakeShader(const char *in, const char *globalVar)
     fclose(f);
 
     fputs("\";\n", output);
+    count++;
 }
 
 int main(void)
 {
     output = fopen("../src/shaders.gen.h", "w");
 
-    MakeShader("../src/quad.vert", "QUAD_VERT");
-    MakeShader("../src/quad.frag", "QUAD_FRAG");
-    MakeShader("../src/sprite.frag", "SPRITE_FRAG");
     MakeShader("../src/texture.vert", "TEXTURE_VERT");
     MakeShader("../src/texture.frag", "TEXTURE_FRAG");
 
     fclose(output);
+
+    printf("Generated %d shaders\n", count);
 }
