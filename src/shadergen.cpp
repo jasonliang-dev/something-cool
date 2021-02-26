756603ca2c8d471817ad331b62eee6587064d1b5
Jason Liang <jasonliang512@gmail.com>
2021-02-25 17:03:07 -0800

    Revert "update shadergen"
    This reverts commit 2d235614554bb8e143ddf45649dff2558d8e6463.

--- src/shadergen.cpp
+++ src/shadergen.cpp
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -7,19 +6,12 @@
 #include "language.h"
 
 global FILE *output;
-global i32 count = 0;
 
 internal void MakeShader(const char *in, const char *globalVar)
 {
     fprintf(output, "global const char *%s = \"\\\n", globalVar);
 
     FILE *f = fopen(in, "r");
-    if (!f)
-    {
-        fprintf(stderr, "Can't open %s\n", in);
-        fclose(output);
-        exit(1);
-    }
 
     char line[4096];
     while (fgets(line, ArrayCount(line), f))
@@ -31,17 +23,17 @@ internal void MakeShader(const char *in, const char *globalVar)
     fclose(f);
 
     fputs("\";\n", output);
-    count++;
 }
 
 int main(void)
 {
     output = fopen("../src/shaders.gen.h", "w");
 
+    MakeShader("../src/quad.vert", "QUAD_VERT");
+    MakeShader("../src/quad.frag", "QUAD_FRAG");
+    MakeShader("../src/sprite.frag", "SPRITE_FRAG");
     MakeShader("../src/texture.vert", "TEXTURE_VERT");
     MakeShader("../src/texture.frag", "TEXTURE_FRAG");
 
     fclose(output);
-
-    printf("Generated %d shaders\n", count);
 }
